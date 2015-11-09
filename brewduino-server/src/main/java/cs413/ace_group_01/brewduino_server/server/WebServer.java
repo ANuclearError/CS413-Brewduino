6dfcddb34d18eea49f6812503dd90847c47eb56c
Aidan O'Grady <aidan@aidanogrady.com>
2015-11-09 10:50:09 +0000

    I have the server accepting requests, need to now actually do something with them.

--- brewduino-server/src/main/java/cs413/ace_group_01/brewduino_server/server/WebServer.java
+++ brewduino-server/src/main/java/cs413/ace_group_01/brewduino_server/server/WebServer.java
@@ -1,6 +1,12 @@
 package cs413.ace_group_01.brewduino_server.server;
 
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * The WebServer class is the main class of the system. It contains the main
  * method and is responsible for listening and delegating any requests it is
@@ -12,9 +18,24 @@ package cs413.ace_group_01.brewduino_server.server;
  */public class WebServer implements Runnable {
 
     /**
+     * Port number the server is listening on.
+     */
+    private static final int PORT = 6789;
+
+    /**
      * A toggle for whether or not the server is actively listening.
      */
     private boolean run = true;
+
+    /**
+     * THe server socket listening for incoming traffic.
+     */
+    private ServerSocket serverSocket;
+
+    /**
+     * Thread pool handling the threads for each request.
+     */
+    private ExecutorService threadPool;
 
     public void run() {
         open();
@@ -23,7 +44,14 @@ package cs413.ace_group_01.brewduino_server.server;
     }
 
     private void open() {
-        System.out.println("Opening server");
+        try {
+            System.out.println("Opening server");
+            serverSocket = new ServerSocket(PORT);
+            threadPool = Executors.newCachedThreadPool();
+        } catch(IOException e) {
+            System.err.println("Error opening server.");
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -31,7 +59,13 @@ package cs413.ace_group_01.brewduino_server.server;
      */
     private void listen() {
         while(run) {
-
+            try {
+                Socket client = serverSocket.accept();
+                System.out.println("Accepted: " + client.toString());
+            } catch (IOException e) {
+                System.err.println("Error accepting request.");
+                e.printStackTrace();
+            }
         }
         close();
     }
