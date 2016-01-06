e24c19df836a134c8ccb46aa0d70288b8f4054c6
adamus1red <adamus1red@users.noreply.github.com>
2016-01-06 12:41:34 +0000

    fix missnamed servo object

--- arduino/actuatorServo.ino
+++ arduino/actuatorServo.ino
@@ -3,19 +3,15 @@
 Servo shotServo;
 #define PIN_SERVO (8)
 #define strokeMax (100)
-
-// Move the actuator to the specified point
+ 
 void SetStrokePerc(float strokePercentage)
 {
   if ( strokePercentage >= 1.0 && strokePercentage <= 99.0 )
   {
     int usec = 1000 + strokePercentage * ( 2000 - 1000 ) / 100.0 ;
-    myServo.writeMicroseconds( usec );
+    shotServo.writeMicroseconds( usec );
   }
 }
-
-// Untested
-// Move the actuator to a certain height (in mm)
 void SetStrokeMM(int strokeReq,int strokeMax)
 {
   SetStrokePerc( ((float)strokeReq) / strokeMax );
@@ -24,32 +20,23 @@ void SetStrokeMM(int strokeReq,int strokeMax)
  
 void setup() 
 { 
-  myServo.attach(PIN_SERVO);
+  shotServo.attach(PIN_SERVO);
 } 
   
  
 void loop() 
 { 
 
-// Test move to 60% (Current shot trigger value)
   SetStrokePerc(60);
   delay(3000);
-  
-// Reset to lowest height
   SetStrokePerc(1);
   delay(3000);
-  
-// Move to top of stroke 
   SetStrokePerc(99);
   delay(3000);
   
-// Reset to lowest height
-  SetStrokePerc(1);
-  delay(3000);
   
-// increment between highest and lowest in increments of 10%
   int d = 10;
-  int delayMS = 1500;
+  int delayMS = 1000;
   int i = 0;
   for ( i = 1; i < 99; i += d )
   {
